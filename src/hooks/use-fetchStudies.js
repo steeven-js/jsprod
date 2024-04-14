5f74fed12b735528f7c2dc5367337eda2c6c7aa0
steeven-js <jacques.steeven@gmail.com>
2024-04-14 14:09:59 -0400

    Mise à jour de la récupération des études de cas et de la vue marketing

---
+++ src/hooks/use-fetchStudies.js
@@ -0,0 +1,64 @@
+import { useState, useEffect, useCallback } from "react";
+
+const useFetchStudies = () => {
+  const [studies, setStudies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isStudiesLoading, setIsStudiesLoading] = useState(false);
+  const [studiesError, setStudiesError] = useState(null);
+  const [studyCoverUrls, setStudyCoverUrls] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    setIsStudiesLoading(true);
+
+    try {
+      const endpoint = `http://127.0.0.1:8000/api/categories?page=${currentPage}`;
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error('La réponse du réseau n\'était pas valide');
+      }
+      const result = await response.json();
+
+      setStudies(result.data);
+
+      // Récupérer les URLs des images des posts
+      const coverUrlsPromises = result.data.map(study =>
+        study.media && study.media.length > 0 ? study.media[0].original_url : '/assets/images/marketing/marketing_1.jpg'
+      );
+
+      // Attendre à la fois la réponse du fetch et les URLs des images des posts
+      const _studyCoverUrls = await Promise.all(coverUrlsPromises);
+
+      // Définir les URLs des images des posts
+      setStudyCoverUrls(_studyCoverUrls);
+
+      // Mettre à jour le nombre total de pages
+      setTotalPages(result.last_page);
+
+      // Tags
+      // setTags(result.tags.map(tag => tag.name.fr));
+
+    } catch (error) {
+      setStudiesError(error.message);
+      console.error(error);
+    } finally {
+      setIsStudiesLoading(false);
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, currentPage]);
+
+  return {
+    studies,
+    isStudiesLoading,
+    studiesError,
+    currentPage,
+    totalPages,
+    setCurrentPage,
+    studyCoverUrls
+  };
+}
+
+export default useFetchStudies;
--- src/pages/marketing/case-studies.jsx
+++ src/pages/marketing/case-studies.jsx
@@ -1,10 +1,17 @@
 import { Helmet } from 'react-helmet-async';
 
+import useFetchStudies from 'src/hooks/use-fetchStudies';
+
 import MarketingCaseStudiesView from 'src/sections/_marketing/view/marketing-case-studies-view';
 
 // ----------------------------------------------------------------------
 
 export default function MarketingCaseStudiesPage() {
+
+  const { studies, isStudiesLoading, studiesError, currentPage, totalPages, setCurrentPage, studyCoverUrls } = useFetchStudies();
+
+  console.log('studies', studies);
+
   return (
     <>
       <Helmet>
