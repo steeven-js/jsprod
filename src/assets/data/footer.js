d6b9d9956a9125cea51ab6a9ae6ddf3da9a7a892
steeven-js <jacques.steeven@gmail.com>
2024-04-20 23:08:35 -0400

    Mise à jour du composant footer pour ajouter des informations sur la communauté et les réseaux sociaux

--- src/assets/data/footer.js
+++ src/assets/data/footer.js
@@ -1,9 +1,36 @@
 export const _Footer = [
   { value: '© 2024. All rights reserved', label: '© 2024. All rights reserved' },
   {
-    value: 'The starting point for your next project based on easy-to-customize Material-UI © helps you build apps faster and better.',
-    label: 'The starting point for your next project based on easy-to-customize Material-UI © helps you build apps faster and better.' },
-    { value: 'Nullam tincidunt adipiscing enim', label: 'Explorez nos projets passés pour voir notre expertise en action.' },
-    { value: 'Help Center', label: 'Help Center' },
-    { value: 'Terms of Service', label: 'Terms of Service' },
+    value: 'Créez des applications exceptionnelles avec notre expertise en conception. Votre succès commence ici.',
+    label: 'Créez des applications exceptionnelles avec notre expertise en conception. Votre succès commence ici.'
+  },
+  { value: 'Community', label: 'Community' },
+  { value: 'Documentation', label: 'Documentation' },
+  { value: 'Changelog', label: 'Changelog' },
+  { value: 'Contributing', label: 'Contributing' },
+  { value: 'Help Center', label: 'Help Center' },
+  { value: 'Terms of Service', label: 'Terms of Service' },
 ];
+
+export const _socialsFooter = [
+  {
+    value: 'facebook',
+    label: 'FaceBook',
+    icon: 'carbon:logo-facebook',
+    color: '#1877F2',
+  },
+  {
+    value: 'instagram',
+    label: 'Instagram',
+    icon: 'carbon:logo-instagram',
+    color: '#E02D69',
+  },
+  {
+    value: 'linkedin',
+    label: 'Linkedin',
+    icon: 'carbon:logo-linkedin',
+    color: '#007EBB',
+  },
+];
+
+// ----------------------------------------------------------------------
--- src/layouts/main/footer.jsx
+++ src/layouts/main/footer.jsx
@@ -4,21 +4,17 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Collapse from '@mui/material/Collapse';
-import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { alpha, styled } from '@mui/material/styles';
-import InputAdornment from '@mui/material/InputAdornment';
-import Button, { buttonClasses } from '@mui/material/Button';
 
 import { usePathname } from 'src/routes/hooks';
 import { RouterLink } from 'src/routes/components';
 
 import { useBoolean } from 'src/hooks/use-boolean';
 
-import { _socials } from 'src/_mock';
+import { _Footer, _socialsFooter } from 'src/assets/data/footer';
 
 import Logo from 'src/components/logo';
 import Iconify from 'src/components/iconify';
@@ -26,16 +22,16 @@ import Iconify from 'src/components/iconify';
 
 // ----------------------------------------------------------------------
 
-const StyledAppStoreButton = styled(Button)(({ theme }) => ({
-  flexShrink: 0,
-  padding: '5px 12px',
-  color: theme.palette.common.white,
-  border: `solid 1px ${alpha(theme.palette.common.black, 0.24)}`,
-  background: `linear-gradient(180deg, ${theme.palette.grey[900]} 0%, ${theme.palette.common.black} 100%)`,
-  [`& .${buttonClasses.startIcon}`]: {
-    marginLeft: 0,
-  },
-}));
+// const StyledAppStoreButton = styled(Button)(({ theme }) => ({
+//   flexShrink: 0,
+//   padding: '5px 12px',
+//   color: theme.palette.common.white,
+//   border: `solid 1px ${alpha(theme.palette.common.black, 0.24)}`,
+//   background: `linear-gradient(180deg, ${theme.palette.grey[900]} 0%, ${theme.palette.common.black} 100%)`,
+//   [`& .${buttonClasses.startIcon}`]: {
+//     marginLeft: 0,
+//   },
+// }));
 
 // ----------------------------------------------------------------------
 
@@ -57,7 +53,7 @@ export default function Footer() {
       <Logo single />
 
       <Typography variant="caption" component="div" sx={{ color: 'text.secondary' }}>
-        © 2024. All rights reserved
+        {_Footer[0].label}
       </Typography>
     </Container>
   );
@@ -79,27 +75,26 @@ export default function Footer() {
                 <Logo />
 
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                  The starting point for your next project based on easy-to-customize Material-UI ©
-                  helps you build apps faster and better.
+                  {_Footer[1].label}
                 </Typography>
               </Stack>
 
               <Stack spacing={1} alignItems="flex-start">
-                <Typography variant="h6">Community</Typography>
+                <Typography variant="h6">{_Footer[2].label}</Typography>
                 <Link variant="body2" sx={{ color: 'text.primary' }}>
-                  Documentation
+                  {_Footer[3].label}
                 </Link>
 
                 <Link variant="body2" sx={{ color: 'text.primary' }}>
-                  Changelog
+                  {_Footer[4].label}
                 </Link>
 
                 <Link variant="body2" sx={{ color: 'text.primary' }}>
-                  Contributing
+                  {_Footer[5].label}
                 </Link>
               </Stack>
 
-              <Stack spacing={2}>
+              {/* <Stack spacing={2}>
                 <Stack spacing={1}>
                   <Typography variant="h6">Let’s stay in touch</Typography>
                   <Typography variant="caption" sx={{ color: 'text.secondary' }}>
@@ -121,12 +116,12 @@ export default function Footer() {
                     ),
                   }}
                 />
-              </Stack>
+              </Stack> */}
 
               <Stack spacing={2}>
                 <Typography variant="h6">Social</Typography>
                 <Stack direction="row" alignItems="center">
-                  {_socials.map((social) => (
+                  {_socialsFooter.map((social) => (
                     <IconButton key={social.value} color="primary">
                       <Iconify icon={social.icon} />
                     </IconButton>
@@ -134,10 +129,10 @@ export default function Footer() {
                 </Stack>
               </Stack>
 
-              <Stack spacing={2}>
+              {/* <Stack spacing={2}>
                 <Typography variant="h6">Apps</Typography>
                 <AppStoreButton />
-              </Stack>
+              </Stack> */}
             </Stack>
           </Grid>
 
@@ -169,16 +164,16 @@ export default function Footer() {
           sx={{ py: 3, textAlign: 'center' }}
         >
           <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-            © 2024. All rights reserved
+            {_Footer[0].label}
           </Typography>
 
           <Stack direction="row" spacing={3} justifyContent="center">
             <Link variant="caption" sx={{ color: 'text.secondary' }}>
-              Help Center
+              {_Footer[6].label}
             </Link>
 
             <Link variant="caption" sx={{ color: 'text.secondary' }}>
-              Terms of Service
+              {_Footer[7].label}
             </Link>
           </Stack>
         </Stack>
@@ -296,32 +291,32 @@ ListMobile.propTypes = {
 
 // ----------------------------------------------------------------------
 
-function AppStoreButton({ ...other }) {
-  return (
-    <Stack direction="row" flexWrap="wrap" spacing={2} {...other}>
-      <StyledAppStoreButton startIcon={<Iconify icon="ri:apple-fill" width={28} />}>
-        <Stack alignItems="flex-start">
-          <Typography variant="caption" sx={{ opacity: 0.72 }}>
-            Download on the
-          </Typography>
+// function AppStoreButton({ ...other }) {
+//   return (
+//     <Stack direction="row" flexWrap="wrap" spacing={2} {...other}>
+//       <StyledAppStoreButton startIcon={<Iconify icon="ri:apple-fill" width={28} />}>
+//         <Stack alignItems="flex-start">
+//           <Typography variant="caption" sx={{ opacity: 0.72 }}>
+//             Download on the
+//           </Typography>
 
-          <Typography variant="h6" sx={{ mt: -0.5 }}>
-            Apple Store
-          </Typography>
-        </Stack>
-      </StyledAppStoreButton>
+//           <Typography variant="h6" sx={{ mt: -0.5 }}>
+//             Apple Store
+//           </Typography>
+//         </Stack>
+//       </StyledAppStoreButton>
 
-      <StyledAppStoreButton startIcon={<Iconify icon="logos:google-play-icon" width={28} />}>
-        <Stack alignItems="flex-start">
-          <Typography variant="caption" sx={{ opacity: 0.72 }}>
-            Download from
-          </Typography>
+//       <StyledAppStoreButton startIcon={<Iconify icon="logos:google-play-icon" width={28} />}>
+//         <Stack alignItems="flex-start">
+//           <Typography variant="caption" sx={{ opacity: 0.72 }}>
+//             Download from
+//           </Typography>
 
-          <Typography variant="h6" sx={{ mt: -0.5 }}>
-            Google Play
-          </Typography>
-        </Stack>
-      </StyledAppStoreButton>
-    </Stack>
-  );
-}
+//           <Typography variant="h6" sx={{ mt: -0.5 }}>
+//             Google Play
+//           </Typography>
+//         </Stack>
+//       </StyledAppStoreButton>
+//     </Stack>
+//   );
+// }
