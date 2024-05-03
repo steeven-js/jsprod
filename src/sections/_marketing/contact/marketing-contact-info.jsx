bc3d92cd9789e2ef88f0cd29e2193657927f8363
steeven-js <jacques.steeven@gmail.com>
2024-05-03 03:33:07 -0400

    Suppression des composants ScrollProgress, Icons, Image, Label, Components, Player, Scroll, Animate, Carousel, Lightbox, CountUp, MegaMenu et Markdown

--- src/sections/_marketing/contact/marketing-contact-info.jsx
+++ src/sections/_marketing/contact/marketing-contact-info.jsx
@@ -19,7 +19,7 @@ export default function MarketingContactInfo() {
       {mdUp && (
         <Image
           alt="marketing contact"
-          src="/assets/illustrations/illustration_marketing_contact.svg"
+          src="/assets/illustrations/illustration_marketing_contact_1.svg"
           sx={{ maxWidth: 380 }}
         />
       )}
@@ -31,9 +31,9 @@ export default function MarketingContactInfo() {
           <Stack spacing={1} direction="row" alignItems="center">
             <Typography variant="h6">{_MarketingContactInfo[0].label}</Typography>
 
-            <Link sx={{ lineHeight: 0 }}>
+            {/* <Link sx={{ lineHeight: 0 }}>
               <Iconify icon="carbon:launch" width={18} />
-            </Link>
+            </Link> */}
           </Stack>
 
           <Typography variant="body2">{_MarketingContactInfo[1].label}</Typography>
@@ -44,15 +44,16 @@ export default function MarketingContactInfo() {
         <Iconify width={28} icon="carbon:mobile" />
         <Stack spacing={0.5}>
           <Typography variant="h6">{_MarketingContactInfo[2].label}</Typography>
+          <Typography variant="body2">{_MarketingContactInfo[3].label}</Typography>
         </Stack>
       </Stack>
 
       <Stack spacing={2} alignItems="flex-start" direction="row">
         <Iconify width={28} icon="carbon:email" />
         <Stack spacing={0.5}>
-          <Typography variant="h6">Talk to us</Typography>
-          <Link color="inherit" variant="body2" href={_MarketingContactInfo[3].label}>
-            {_MarketingContactInfo[5].label}
+          <Typography variant="h6">{_MarketingContactInfo[4].label}</Typography>
+          <Link color="inherit" variant="body2" href={_MarketingContactInfo[5].label}>
+            {_MarketingContactInfo[6].label}
           </Link>
         </Stack>
       </Stack>
@@ -60,8 +61,8 @@ export default function MarketingContactInfo() {
       <Stack spacing={2} alignItems="flex-start" direction="row">
         <Iconify width={28} icon="carbon:time" />
         <Stack spacing={0.5}>
-          <Typography variant="h6">{_MarketingContactInfo[6].label}</Typography>
-          <Typography variant="body2">{_MarketingContactInfo[7].label}</Typography>
+          <Typography variant="h6">{_MarketingContactInfo[7].label}</Typography>
+          <Typography variant="body2">{_MarketingContactInfo[8].label}</Typography>
         </Stack>
       </Stack>
     </Stack>
